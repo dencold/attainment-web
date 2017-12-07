aedc06b58f0a246ca5c0ee4438b7b529b846385b
Dennis Coldwell <dennis.coldwell@gmail.com>
2017-12-07 00:31:31 -0800

    first stab at task detail

--- src/components/Dashboard/Views/TaskDetail.vue
+++ src/components/Dashboard/Views/TaskDetail.vue
@@ -1,21 +1,49 @@
 <template>
   <div class="content">
-    <div class="flex-row">
-      <div class="star" @click="toggleStar">
-        <i v-show="task.starred" class="ti-bolt"></i>
-        <i v-show="!task.starred" class="ti-star"></i>
+    <div class="card">
+
+      <div class="flex-row title">
+        <div class="icon" @click="toggleStar">
+          <i v-show="task.completed" class="fa fa-check-square-o"></i>
+          <i v-show="!task.completed" class="fa fa-square-o"></i>
+        </div>
+        <span class="name">{{task.name}}</span>
+        <div class="icon" @click="toggleStar">
+          <i v-show="task.starred" class="fa fa-star"></i>
+          <i v-show="!task.starred" class="fa fa-star-o"></i>
+        </div>
       </div>
-      <span class="name">{{task.name}}</span>
-    </div>
 
-    <div class="flex-row">
-      <span class="project">{{project.name}}</span>
-      <span class="poms">{{task.poms}}</span>
-      <span class="due">{{task.dueAt}}</span>
-    </div>
+      <hr/>
 
-    <div class="flex-row">
-      <code>{{task.details}}</code>
+      <div class="flex-row">
+        <div class="icon" @click="toggleStar">
+          <i class="ti-folder"></i>
+        </div>
+        <span class="name">{{project.name}}</span>
+      </div>
+
+      <div class="flex-row">
+        <div class="icon" @click="toggleStar">
+          <i class="ti-bolt"></i>
+        </div>
+        <span class="name">{{task.poms}}</span>
+      </div>
+
+      <div class="flex-row">
+        <div class="icon" @click="toggleStar">
+          <i class="ti-calendar"></i>
+        </div>
+        <span class="name">{{task.dueAt}}</span>
+      </div>
+
+      <div class="flex-row">
+        <div class="icon" @click="toggleStar">
+          <i class="ti-pencil"></i>
+        </div>
+        <span class="name">{{task.details}}</span>
+      </div>
+
     </div>
   </div>    
 </template>
@@ -60,22 +88,36 @@
   }
 </script>
 
-<style>
+<style scoped>
+  .content {
+    font-size: 16px;
+  }
+  .card {
+    padding: 15px;
+  }
   .flex-row {
-    height: 50px;
+    height: auto;
     display: flex;
     flex-direction: row;
     align-items: center;
     padding: 0 15px 0 15px;
   }
-  .star {
-    width: 25px;
+  .icon {
+    display: flex;
+    width: auto;
+    font-size: 95%;
+    justify-content: center;
+    align-items: baseline;
+  }
+  .title {
+    font-size: 200%;
   }
   .project {
     width: 150px;
   }
   .name {
     flex-grow: 1;
+    padding: 0 10px 0 10px;
   }
   .pom {
     width: 25px;
