63fa1fcb52f7dc5065451e990d6843bedf00c4fc
Dennis Coldwell <dennis.coldwell@gmail.com>
2018-01-04 20:08:00 -0800

    adding prod config

--- src/modules/firebase.js
+++ src/modules/firebase.js
@@ -1,8 +1,9 @@
 import firebase from 'firebase'
 import 'firebase/firestore'
 
+/* eslint-disable */
 // configure firebase
-const config = {
+const devConfig = {
   apiKey: 'AIzaSyBy6wjkoJPJVpysm2ZsFZ6VOxF8FhCtx68',
   authDomain: 'attainment-dev.firebaseapp.com',
   databaseURL: 'https://attainment-dev.firebaseio.com',
@@ -11,6 +12,15 @@ const config = {
   messagingSenderId: '646574338415'
 }
 
-const fireApp = firebase.initializeApp(config)
+const prodConfig = {
+  apiKey: 'AIzaSyDs8E_p1Uyiq9mLcBC6G__Kdzu3ggkgt9I',
+  authDomain: 'attainment-6017d.firebaseapp.com',
+  databaseURL: 'https://attainment-6017d.firebaseio.com',
+  projectId: 'attainment-6017d',
+  storageBucket: 'attainment-6017d.appspot.com',
+  messagingSenderId: '1096403605650'
+}
+
+const fireApp = firebase.initializeApp(prodConfig)
 
 export default fireApp
