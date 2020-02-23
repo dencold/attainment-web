2463a9805de82cc2c436f130c7081443f6c3bddb
Dennis Coldwell <dennis.coldwell@gmail.com>
2020-02-23 10:32:07 -0800

    switched to tiptap for our textarea editor - closes #106

--- src/components/UIComponents/Inputs/TextArea.vue
+++ src/components/UIComponents/Inputs/TextArea.vue
@@ -1,22 +1,68 @@
 <template>
-  <textarea
-    ref="textarea"
-    :placeholder="placeholder"
-    :value="value"
-    @change="update($event.target.value)"
-    @keyup.esc="$refs.textarea.blur()"
-    @keypress.stop
-    @keyup.stop
-  />
+  <div class="editor" @keypress.stop @keyup.stop>
+    <editor-content
+      class="editor__content"
+      :editor="editor"
+    />
+  </div>
 </template>
 
 <script>
+  import { Editor, EditorContent } from 'tiptap'
+  import {
+    Blockquote,
+    CodeBlock,
+    HardBreak,
+    Heading,
+    OrderedList,
+    BulletList,
+    ListItem,
+    TodoItem,
+    TodoList,
+    Bold,
+    Code,
+    Italic,
+    Link,
+    History,
+  } from 'tiptap-extensions'
+
   export default {
+    components: {
+      EditorContent
+    },
+
     props: {
       placeholder: String,
       value: String
     },
-
+    data() {
+      return {
+        editor: new Editor({
+          extensions: [
+            new Blockquote(),
+            new BulletList(),
+            new CodeBlock(),
+            new HardBreak(),
+            new Heading({ levels: [1, 2, 3] }),
+            new ListItem(),
+            new OrderedList(),
+            new TodoItem(),
+            new TodoList(),
+            new Bold(),
+            new Code(),
+            new Italic(),
+            new Link(),
+            new History(),
+          ],
+          content: this.value,
+          onUpdate: ({ getHTML }) => {
+              // get new content on update
+              const newContent = getHTML()
+              this.$emit('updated', newContent)
+          },
+        })
+      }
+    },
     methods: {
       update (newVal) {
         this.$emit('updated', newVal)
@@ -24,14 +70,18 @@
       focus () {
         this.$refs.textarea.focus()
       }
+    },
+    beforeDestroy() {
+      // Always destroy your editor instance when it's no longer needed
+      this.editor.destroy()
     }
   }
 
 </script>
 <style scoped>
-  textarea {
+  .editor__content {
     width: 100%;
-    height: 100px;
+    min-height: 100px;
     padding: 10px;
     background-color: #F5F5F5;
     border: none;
